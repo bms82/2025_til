7040c7b4fc9ff792285bd059673a72798ce708d6
bms82 <bang.moonsung82@gmail.com>
2025-01-23 20:24:51 +0900

    Initial commit

---
+++ src/test/java/per/msb/til/lessons/LessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package per.msb.til.lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
